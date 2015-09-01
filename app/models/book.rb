219627ac58d893cf30b10c66d327de8a58fe464d
Phil Alekseev <phil.alekseev@gmail.com>
2015-09-01 08:44:54 -0400

    Create Sales Model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
   belongs_to :user
+  has_many :sales
   has_attached_file :image
   has_attached_file :resource
 
@@ -13,4 +14,7 @@ class Book < ActiveRecord::Base
 
   validates :image, attachment_presence: true
   validates :resource, attachment_presence: true
+
+  validates_numericality_of :price,
+  greater_than: 49, message: "Must be at least 50 cents."
 end
